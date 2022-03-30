3aae86dc1e711a103f9ff560bba8ff46d55a3be5
Dmytro Vovk <dmitry.vovk@gmail.com>
2022-03-30 15:52:34 +0100

    Add support for Darwin platform

--- internal/detector/darwin.go
+++ internal/detector/darwin.go
@@ -1,8 +1,46 @@
 //go:build darwin
-// +build darwin
 
 package detector
 
-func FindPID() {
-	// TODO
+/*
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <libproc.h>
+*/
+import "C"
+import (
+	"github.com/mitchellh/go-ps"
+	"path/filepath"
+	"strconv"
+	"unsafe"
+)
+
+func FindPID(name string) (string, error) {
+	processes, err := ps.Processes()
+	if err != nil {
+		return "", nil
+	}
+
+	baseName := filepath.Base(name)
+	buf := C.malloc(C.PROC_PIDPATHINFO_MAXSIZE)
+	defer C.free(unsafe.Pointer(buf))
+
+	for i := range processes {
+		if baseName != processes[i].Executable() {
+			continue
+		}
+
+		ret := C.proc_pidpath(C.int(processes[i].Pid()), buf, C.PROC_PIDPATHINFO_MAXSIZE)
+		if ret < 0 {
+			continue
+		}
+
+		binaryFullName := string(C.GoBytes(unsafe.Pointer(buf), ret))
+		if binaryFullName == name {
+			return strconv.Itoa(processes[i].Pid()), nil
+		}
+	}
+
+	return "", nil
 }
